5c4f2afaa97e3f5518a63b1194f51b0870532ae2
SachinGoud9 <82360963+SachinGoud9@users.noreply.github.com>
2021-07-13 09:42:38 +0530

    Update RegexPattern.java

--- src/JavaAssignment9/RegexPattern.java
+++ src/JavaAssignment9/RegexPattern.java
@@ -10,12 +10,12 @@ public class RegexPattern {
 		//Regex Patterns
 		String regex = "[A-Z][A-Za-z0-9_!@#\\$%\\^\\&*\\)\\(+=.\\s+]*[.]$";
 		Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            System.out.println("Match found");
-        } else {
-            System.out.println("Match not found");
-        }
+        	Matcher matcher = pattern.matcher(input);
+        	boolean matchFound = matcher.find();
+        	if (matchFound) {
+            		System.out.println("Match found");
+        	} else {
+            		System.out.println("Match not found");
+        	}
 	}
 }
