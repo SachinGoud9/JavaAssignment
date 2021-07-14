5dabb08a16857f7103ff855d2bc2896ac8d094aa
SachinGoud9 <82360963+SachinGoud9@users.noreply.github.com>
2021-07-14 20:05:17 +0530

    Update IntChar.java

--- src/SachinGoud/assignment/data/IntChar.java
+++ src/SachinGoud/assignment/data/IntChar.java
@@ -1,20 +1,20 @@
 package SachinGoud.assignment.data;
 
 public class IntChar {
-	int instanceInt;
-	char instanceChar;
+	int intanceIntegerVariable;
+	char instanceCharacterVariable;
 	
 	//Function to display Uninitialized Instance Variables.
 	public void displayInstanceVariables() {
-		System.out.println("Value of unintialised Instance Integer variable: "+instanceInt);
-		System.out.println("Value of unintialised Instance Character variable: "+instanceChar);
+		System.out.println("Value of unintialised Instance Integer variable: "+intanceIntegerVariable);
+		System.out.println("Value of unintialised Instance Character variable: "+instanceCharacterVariable);
 	}
 	
 	//Function to display uninitialized Local variables.
 	public void displayLocalVariables() {
 		int localInt;
 		char localChar;
-		System.out.println("Value of unintialised Local Integer variable: "+instanceInt);
-		System.out.println("Value of unintialised Local Character variable: "+instanceChar);
+		System.out.println("Value of unintialised Local Integer variable: "+intanceIntegerVariable);
+		System.out.println("Value of unintialised Local Character variable: "+instanceCharacterVariable);
 	}
 }
