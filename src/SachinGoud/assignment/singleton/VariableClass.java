71d4454874d7b7ea86b914e3228b35dafe174fdd
SachinGoud9 <82360963+SachinGoud9@users.noreply.github.com>
2021-07-14 20:16:18 +0530

    Update VariableClass.java

--- src/SachinGoud/assignment/singleton/VariableClass.java
+++ src/SachinGoud/assignment/singleton/VariableClass.java
@@ -1,18 +1,18 @@
 package SachinGoud.assignment.singleton;
 
 public class VariableClass {
-	String str;
+	String variableString;
 	//public static VariableClass initializeString(String temp) {
 		//You cannot refer non static variables or methods from static method. 
-		//Here "str" is a static variable hence it cannot be refered.
-		//str = temp;
+		//Here "variableString" is a static variable hence it cannot be refered.
+		//variableString = temp;
 		
 		//Static methods deal with classes not objects. Hence, you cannot return an object from a static method.
 		//return this;		
 	//}
 	//Function to print the String.
 	public VariableClass display() {
-		System.out.println(str);
+		System.out.println(variableString);
 		return this;
 	}
 }
